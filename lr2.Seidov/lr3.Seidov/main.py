5147b82f3d28e916b54a75f5d23911c5535e6cd3
Tagirss <114563809+Tagirss@users.noreply.github.com>
2025-04-07 00:23:25 +0300

    Delete lr2.Seidov/lr3.Seidov directory

--- lr2.Seidov/lr3.Seidov/main.py
+++
@@ -1,8 +0,0 @@
-from func import *
-print_credits("leonardo", "maxim", "вика", "саша")
-print_check(майонез = 200, сыр = 400, огурцы = 250, сметана = 150)
-print_dict("приветики", get_symbol_count)
-mult_by_9 = multiply(9)
-mult_by_9(10)
-if __name__ == "__main__":
-    print("Hello, World!")
