23dae06b1dc6638589f3b003f3920994a4f15053
Tagirss <114563809+Tagirss@users.noreply.github.com>
2025-04-07 00:25:23 +0300

    Add files via upload

---
+++ lr7.Seidov/main.py
@@ -0,0 +1,13 @@
+from strings import *
+if __name__ == "__main__":
+    print(formatted_string("Alice", 5, 10))
+    print(repeat_combination("Hello", "World", 3))
+    print(count_substring("Hello World, hello world!", "hello"))
+    print(substring_between("Hello World", 1, 5))
+    print(find_latin_in_cyrillic("Привет", "Hello", "Кириллица и латиница together"))
+    print(is_palindrome("A man, a plan, a canal, Panama!"))
+    print(trim_and_measure("   Hello   World    "))
+    print(replace_endings_with_newline("Hello world! How are you? I'm fine."))
+    print(remove_digits("Hello123 World456"))
+    print(replace_spaces("Hello World"))
+    print(longest_word("Find the longest word in this sentence"))
